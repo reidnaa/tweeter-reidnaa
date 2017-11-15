2896b1043bb4b42a544d0e1d37ccd8c04b058e06
reidnaa <reidnaaykens@yahoo.ca>
2017-11-15 02:50:21 +0000

    tweeter - creating the tweets module

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+  console.log('document loaded');
+
+  var maxChar = 140;
+
+  var text_input = $('.text-input');
+  var counter = $('.counter');
+
+  counter.text(maxChar);
+
+  text_input.keyup(function(){
+    var characters = $(this).val().length ;
+     counter.text(maxChar - characters );
+
+if (characters > maxChar){
+      counter.addClass('go-red');
+} else {
+  counter.removeClass('go-red');
+}
+
+  });
+
+});
